bb54d8b705d1ea175d6a3f32ddb299518ec094e6
Avantika srivastava <Avantika.27832@stu.upes.ac.in>
2025-08-30 18:29:24 +0530

    Add files via upload

---
+++ day1a.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int a,b;
+printf("enter no. 1 ");
+scanf("%d", &a);
+printf(" enter no.2 ");
+scanf("%d", &b);
+int sum= a+b;
+printf("the sum of the numbers entered: %d", sum);
+return 0;
+}
