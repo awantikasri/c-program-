5b14a6f9fc9e2778ec7bfd7a2a6d684f813fa726
Avantika srivastava <Avantika.27832@stu.upes.ac.in>
2025-09-03 16:28:17 +0530

    Add files via upload

---
+++ day4a.c
@@ -0,0 +1,16 @@
+//Write a program to swap two numbers without using a third variable.
+
+#include<stdio.h>
+int main() 
+{ int a,b;
+printf("enter number 1: ");
+scanf("%d", &a);
+printf("enter number 2: ");
+scanf("%d",&b);
+printf("original sequece: %d %d \n", a,b);
+a=a-b;
+b= a+b;
+a=b-a;
+printf("swapped  numbers: %d %d ", a,b);
+return 0;
+}
