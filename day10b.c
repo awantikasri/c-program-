b24755a4a6c02fe96bf0826a76e2619e101cd96f
Avantika srivastava <Avantika.27832@stu.upes.ac.in>
2025-10-15 00:58:09 +0530

    Add files via upload

---
+++ day10b.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{ int i;
+printf("enter day number please ");
+scanf("%d",&i);
+switch(i)
+{
+case 1:
+printf("monday");
+break;
+case 2: 
+printf("tuesday");
+break;
+case 3:
+printf("wednesday");
+break;
+case 4:
+printf("thrusday");
+break;
+case 5:
+printf("friday");
+break;
+case 6: 
+printf("saturday");
+break;
+case 7:
+printf("sunday");
+default: 
+printf("invalid day number ");
+}
+return 0;
+}
