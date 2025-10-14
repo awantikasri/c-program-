5daf12f63af9af6bd79b9c0cf152a5f84fa419fe
Avantika srivastava <Avantika.27832@stu.upes.ac.in>
2025-10-15 01:24:19 +0530

    Add files via upload

---
+++ day15a.c
@@ -0,0 +1,14 @@
+//Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main()
+{ int n;
+printf("enter a number: \n");
+scanf("%d",&n);
+int fact=1;
+for(int i=1; i<=n; i++)
+{
+    fact=fact*i;
+}
+printf("factorial of the number is: %d ", fact);
+return 0;
+}
